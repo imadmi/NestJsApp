805538906e061002124c9f70863a2f838da21afd
imad mimouni <imad.mimouni.123@gmail.com>
2023-11-17 16:24:14 +0100

    update

--- src/ningas/ningas.controller.ts
+++ src/ningas/ningas.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Query } from '@nestjs/common';
 
 @Controller('ningas')
-export class NingasController {}
+export class NingasController {
+
+    @Get()
+    // Get http://localhost:3001/ningas?type=fast
+    getNingas(@Query('type') type: string) {
+        return type=='fast' ? 'Fast ningas' : 'Slow ningas';
+    }
+
+    @Get(':id')
+    getNingasById(@Param('id') id: string) {
+        return id;
+    }
+
+    @Post()
+    createNingas() {
+        return 'Create ningas';
+    }
+
+    @Put(':id')
+    updateNingas() {
+        return 'Update ningas';
+    }
+
+    @Delete(':id')
+    deleteNingas() {
+        return 'Delete ningas';
+    }
+}
