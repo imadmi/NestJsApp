7515d73224fc98c7069d0fbd929142be5faf2db5
imad mimouni <imad.mimouni.123@gmail.com>
2023-11-20 02:14:08 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,12 @@ import { NingasModule } from './ningas/ningas.module';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [NingasModule, AuthModule],
+  imports: [NingasModule, AuthModule, UserModule, BookmarkModule, PrismaModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
