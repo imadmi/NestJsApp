01cdcf464feb98a9f41f1323293b2decf68ac98f
imad <imad.mimouni.123@gmail.com>
2023-11-21 21:41:46 +0100

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Get, Post } from '@nestjs/common';
+import { AuthDto }  from './auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authservice: AuthService) {}
 
+  // @Get('signin')
   @Post('signin')
-  signin() {
-    return this.authservice.signin();
+  signin(@Body() dto : AuthDto) {
+    // console.log(dto);
+
+
+    return this.authservice.signin(dto);
   }
 
   @Post('signup')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './auth.dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma : PrismaService) {}
-  signin() {
-    return 'signin';
+  async signin(dto: AuthDto) {
+
+    const hash = await argon.hash(dto.password);
+    
+    console.log("hada hash " , hash);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email : dto.email,
+        hash,
+      },
+    });
+
+    console.log("hada useer " , user);
+    return user;
   }
 
   signup() {
